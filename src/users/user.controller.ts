fb9f85be0fdf963b4865e5a1bed40509652f06b8
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-03 09:50:55 -0300

    corrections

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dto';
 
@@ -7,21 +16,25 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':userId')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('userId') userId: string) {
     return this.userService.findById(userId);
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() body: CreateUserDTO) {
     return this.userService.create(body);
   }
 
   @Delete(':userId')
+  @HttpCode(HttpStatus.OK)
   delete(@Param('userId') userId: string) {
     return this.userService.delete(userId);
   }
