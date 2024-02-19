df3c220ea22c1b6684169521508e15365c6760fb
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-19 17:33:09 -0300

    create new user methods and tests

--- src/domain/user/user.spec.ts
+++ src/domain/user/user.spec.ts
@@ -1,7 +1,32 @@
 import { User, UserProps } from './user';
 
 describe('User Unit Test', () => {
-  it('should create a user', () => {
+  it('should construtor()', () => {
+    let userProps: UserProps = {
+      name: 'John',
+      surname: 'Doe',
+      email: 'teste@teste.com',
+      password: '123',
+    };
+
+    let user = User.create(userProps);
+
+    userProps = {
+      name: 'John',
+      surname: 'Doe',
+      email: 'teste@teste.com',
+      password: '123',
+    };
+
+    expect(user.id).toBeDefined();
+    user = User.create(userProps);
+
+    expect(user.props).toEqual({
+      ...userProps,
+    });
+  });
+
+  it('should be updateName method', () => {
     const userProps: UserProps = {
       name: 'John',
       surname: 'Doe',
@@ -10,7 +35,46 @@ describe('User Unit Test', () => {
     };
 
     const user = User.create(userProps);
-    expect(user.props).toEqual({ ...userProps });
+    user.updateName('Johnson');
+    expect(user.name).toBe('Johnson');
+  });
+
+  it('should be updateSurname method', () => {
+    const userProps: UserProps = {
+      name: 'John',
+      surname: 'Doe',
+      email: 'teste@teste.com',
+      password: '123',
+    };
+    const user = User.create(userProps);
+    user.updateSurname('Doe Johnson');
+    expect(user.surname).toBe('Doe Johnson');
+  });
+
+  it('should be updateEmail method', () => {
+    const userProps: UserProps = {
+      name: 'John',
+      surname: 'Doe',
+      email: 'teste@teste.com',
+      password: '123',
+    };
+
+    const user = User.create(userProps);
+    user.updateEmail('johndue@teste.com');
+    expect(user.email).toBe('johndue@teste.com');
+  });
+
+  it('should be updatePassword method', () => {
+    const userProps: UserProps = {
+      name: 'John',
+      surname: 'Doe',
+      email: 'teste@teste.com',
+      password: '123',
+    };
+
+    const user = User.create(userProps);
+    user.updatePassword('1234');
+    expect(user.password).toBe('1234');
   });
 
   it('should be toJSON() method', () => {
